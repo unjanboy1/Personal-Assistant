665d50bd2a4bfd3fc06c75b159a52ac6ee802cb8
unjanboy1 <malikshahzaib1515@gmail.com>
2026-06-24 15:15:50 +0500

    key mouse

---
+++ automation/keyboard.py
@@ -0,0 +1,69 @@
+"""
+browser.py
+
+Browser automation for VoicePilot.
+"""
+
+import webbrowser
+from urllib.parse import quote
+
+
+class BrowserController:
+
+    def open_google(self):
+
+        webbrowser.open("https://www.google.com")
+
+        print("Google opened.")
+
+    def open_website(self, url):
+
+        if not url.startswith("http"):
+            url = "https://" + url
+
+        webbrowser.open(url)
+
+        print(f"Opened {url}")
+
+    def search_google(self, query):
+
+        query = quote(query)
+
+        url = f"https://www.google.com/search?q={query}"
+
+        webbrowser.open(url)
+
+        print(f"Searching: {query}")
+
+
+if __name__ == "__main__":
+
+    browser = BrowserController()
+
+    while True:
+
+        print("\n===== Browser Test =====")
+        print("1. Open Google")
+        print("2. Search Google")
+        print("3. Open Website")
+        print("4. Exit")
+
+        choice = input("> ")
+
+        if choice == "1":
+            browser.open_google()
+
+        elif choice == "2":
+
+            query = input("Search : ")
+
+            browser.search_google(query)
+
+        elif choice == "3":
+
+            site = input("Website : ")
+
+            browser.open_website(site)
+
+        elif choice == "4":
+            break
---
+++ automation/system.py
@@ -0,0 +1,94 @@
+"""
+mouse.py
+
+Mouse automation for VoicePilot.
+"""
+
+import pyautogui
+
+
+class MouseController:
+
+    def click(self):
+
+        pyautogui.click()
+
+        print("Left Click")
+
+    def double_click(self):
+
+        pyautogui.doubleClick()
+
+        print("Double Click")
+
+    def right_click(self):
+
+        pyautogui.rightClick()
+
+        print("Right Click")
+
+    def move(self, x, y):
+
+        pyautogui.moveTo(x, y, duration=0.4)
+
+        print(f"Moved to ({x}, {y})")
+
+    def scroll_up(self):
+
+        pyautogui.scroll(500)
+
+    def scroll_down(self):
+
+        pyautogui.scroll(-500)
+
+    def drag(self, x, y):
+
+        pyautogui.dragTo(x, y, duration=0.5)
+
+
+if __name__ == "__main__":
+
+    mouse = MouseController()
+
+    while True:
+
+        print("\n====== Mouse Test ======")
+        print("1. Click")
+        print("2. Double Click")
+        print("3. Right Click")
+        print("4. Move")
+        print("5. Scroll Up")
+        print("6. Scroll Down")
+        print("7. Exit")
+
+        choice = input("> ")
+
+        if choice == "1":
+
+            mouse.click()
+
+        elif choice == "2":
+
+            mouse.double_click()
+
+        elif choice == "3":
+
+            mouse.right_click()
+
+        elif choice == "4":
+
+            x = int(input("X : "))
+            y = int(input("Y : "))
+
+            mouse.move(x, y)
+
+        elif choice == "5":
+
+            mouse.scroll_up()
+
+        elif choice == "6":
+
+            mouse.scroll_down()
+
+        elif choice == "7":
+            break
